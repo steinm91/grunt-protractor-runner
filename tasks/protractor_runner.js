2a8e21d76528e76c330d4272483a6f48299a6455
miindlek <miindlek@gmail.com>
2021-03-31 11:19:07 +0200

    Add unhandled-rejections string option
    Add string option to set the unhandled-rejections option for the node-process.

--- tasks/protractor_runner.js
+++ tasks/protractor_runner.js
@@ -45,7 +45,7 @@ module.exports = function(grunt) {
     grunt.verbose.writeln("Options: " + util.inspect(opts));
 
     var keepAlive = opts['keepAlive'];
-    var strArgs = ["seleniumAddress", "seleniumServerJar", "seleniumPort", "baseUrl", "rootElement", "browser", "chromeDriver", "chromeOnly", "directConnect", "sauceUser", "sauceKey", "sauceSeleniumAddress", "framework", "frameworkPath", "beforeLaunch", "onPrepare", "webDriverProxy"];
+    var strArgs = ["unhandled-rejections", "seleniumAddress", "seleniumServerJar", "seleniumPort", "baseUrl", "rootElement", "browser", "chromeDriver", "chromeOnly", "directConnect", "sauceUser", "sauceKey", "sauceSeleniumAddress", "framework", "frameworkPath", "beforeLaunch", "onPrepare", "webDriverProxy"];
     var listArgs = ["specs", "exclude", "suite"];
     var boolArgs = ["includeStackTrace", "verbose"];
     var objectArgs = ["params", "capabilities", "cucumberOpts", "mochaOpts"];
